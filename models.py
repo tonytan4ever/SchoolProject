9c282ab07724abace9f1d17c6a31249aa88317e5
Tony Tan <tonytan198211@gmail.com>
2013-05-21 23:11:27 -0400

    Added index page, also models and views as well

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from control import db
 
-#===============================================================================
-# class User(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    username = db.Column(db.String)
-#    message = db.Column(db.String)
-# 
-#    def __init__(self, username, message):
-#        self.username = username
-#        self.message = message
-#===============================================================================
+from control import db,app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    message = db.Column(db.String)
+    
+
+    def __init__(self, username, message):
+        self.username = username
+        self.message = message
--- views.py
+++ views.py
@@ -1,24 +1,7 @@
 from models import *
-
 from control import app
-
 from flask import render_template
 
-#===============================================================================
-# @app.route('/signup', methods=['POST'])
-# def signup():
-#    user = User(request.form['username'], request.form['message'])
-#    db.session.add(user)
-#    db.session.commit()
-#    return redirect(url_for('message', username=user.username))
-# 
-# @app.route('/message/<username>')
-# def message(username):
-#    user = User.query.filter_by(username=username).first_or_404()
-#    return render_template('message.html', username=user.username,
-#                                                    message=user.message)
-#===============================================================================
-    
     
 @app.route('/')
 #@cached(120)  # from 200 req/s to 800 req/s
