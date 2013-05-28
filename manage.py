6276d0caae03fe7126fc0c74502b76715c0f64d6
Tony Tan <tonytan198211@gmail.com>
2013-05-27 23:49:07 -0400

    Add assets command and more db models

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from flask.ext.script import Manager, Shell, Server
-from control import app
+from flask_assets import ManageAssets
+from control import app,assets_env
 
 manager = Manager(app)
+manager.add_command("assets", ManageAssets(assets_env))
 manager.add_command("runserver", Server(host="10.0.2.15", port=5000))
 manager.add_command("shell", Shell())
 manager.run()
