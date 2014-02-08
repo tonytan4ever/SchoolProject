2affc60941c79515ed0049a125d644eafef1a31d
tonytan4ever <tonytan198211@gmail.com>
2014-02-07 21:22:39 -0500

    Add Oauth test for twitter.

---
+++ tests/oauthlib_tests/facebook.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, redirect, url_for, session, request
+from flask_oauthlib.client import OAuth
+
+
+FACEBOOK_APP_ID = '527132627401839'
+FACEBOOK_APP_SECRET = '21174e6e7c44880e7eb360ce52888ca6'
+
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+facebook = oauth.remote_app(
+    'facebook',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'},
+    base_url='https://graph.facebook.com',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth'
+)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
+
+if __name__ == '__main__':
+    app.run(host="10.0.2.15",
+             port=5000)
